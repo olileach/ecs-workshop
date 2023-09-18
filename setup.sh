82973e66591f4c4a52c7decf0ed9963ec3fd7469
Oli Leach <oliverle@amazon.co.uk>
2023-09-18 17:16:50 +0100

    Added setup.sh echo output

--- setup.sh
+++ setup.sh
@@ -14,26 +14,25 @@
 "#!/bin/bash"
 
 clear
-echo "Setting up environment - please wait."
+echo -e "\nSetting up environment - please wait.\n"
 export AWS_DEFAULT_OUTPUT="json"
 export AWS_PAGER="" 
 region=$(curl -s http://169.254.169.254/latest/meta-data/placement/region) 
 account_id=$(aws sts get-caller-identity --query Account --output text)
-echo ""
-echo "Configuring Cloud9 instance for immersion day"
+echo -e "\nConfiguring Cloud9 instance for immersion day.\n"
 sudo yum install -y jq
-echo ""
-echo "Installing the latest version of the AWS Cli"
+echo -e "\nInstalling the latest version of the AWS Cli.\n"
 curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "/tmp/awscliv2.zip"
-echo "Unzipping awscliv2.zip to /tmp/cli"
+echo -e "Unzipping awscliv2.zip to /tmp/cli"
 unzip -o -q /tmp/awscliv2.zip -d /tmp/cli
-echo "Updating AWS CLI using awscliv2.zip in /tmp/cli"
+echo -e "Updating AWS CLI using awscliv2.zip in /tmp/cli"
 sudo /tmp/cli/aws/install --update
 aws configure set default.region ${region}
+echo -e "\nSetting up ECR repos - please wait.\n"
 aws ecr create-repository --repository-name main --image-scanning-configuration scanOnPush=false --image-tag-mutability MUTABLE --region ${region}
 aws ecr create-repository --repository-name blue --image-scanning-configuration scanOnPush=false --image-tag-mutability MUTABLE --region ${region}
 aws ecr create-repository --repository-name red --image-scanning-configuration scanOnPush=false --image-tag-mutability MUTABLE --region ${region}
-cd ecsworkshop
+echo -e "\nBuilding Docker images - please wait.\n"
 docker build -t main main/.
 docker build -t red red/.
 docker build -t blue blue/.
@@ -44,3 +43,4 @@ docker tag blue:latest ${account_id}.dkr.ecr.${region}.amazonaws.com/blue:latest
 docker push ${account_id}.dkr.ecr.${region}.amazonaws.com/blue:latest
 docker tag red:latest ${account_id}.dkr.ecr.${region}.amazonaws.com/red:latest
 docker push ${account_id}.dkr.ecr.${region}.amazonaws.com/red:latest
+echo -e "\nSetup complete. Please follow the ecs workshop instructions to complete the workshop.\n"
